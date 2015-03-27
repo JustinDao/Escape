55e8cbabf03c57141cc37dd57017b77d290e2957
Andy Barron <AndrewLBarron@gmail.com>
2015-03-26 22:01:36 -0400

    Cross-platform Program.cs

--- Escape/Program.cs
+++ Escape/Program.cs
@@ -2,6 +2,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+#if WINDOWS || LINUX
+#else
+using MonoMac.AppKit;
+using MonoMac.Foundation;
+#endif
 #endregion
 
 namespace Escape
@@ -22,5 +27,35 @@ namespace Escape
                 game.Run();
         }
     }
+#else
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			NSApplication.Init ();
+
+			using (var p = new NSAutoreleasePool ()) {
+				NSApplication.SharedApplication.Delegate = new AppDelegate ();
+
+				NSApplication.Main (args);
+			}
+		}
+	}
+
+	class AppDelegate : NSApplicationDelegate
+	{
+		private MainGame game;
+
+		public override void FinishedLaunching (MonoMac.Foundation.NSObject notification)
+		{
+			game = new MainGame ();
+			game.Run ();
+		}
+
+		public override bool ApplicationShouldTerminateAfterLastWindowClosed (NSApplication sender)
+		{
+			return true;
+		}
+	}
 #endif
 }
