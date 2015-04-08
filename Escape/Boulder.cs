32f2a24015963d6525cd8500102c91a0f77668e3
Justin <tjd5at@virginia.edu>
2015-04-08 09:04:28 -0400

    Add Initial Boulder Class

---
+++ Escape/Boulder.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Escape
+{
+    class Boulder : SpriteEntity
+    {
+        public Boulder(ContentManager cm, string spriteName, Vector2 position)
+            : base(cm, spriteName)
+        {
+            Origin = new Vector2(0.5f);
+            Position = position;
+        }
+    }
+}
