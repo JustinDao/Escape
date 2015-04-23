a91d159fc3165acd88c1461eff9f31b5b031e01b
Elsie <lw6gk@virginia.edu>
2015-04-23 01:02:00 -0400

    Addsrooms derp

---
+++ Escape/RoomSub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Escape
+{
+    class RoomSub : Room
+    {
+        public RoomSub(MainGame mg, Castle castle)
+            : base(mg, castle, "SubmissionRoom.csv")
+        {
+
+        }
+
+    }
+}
